f3ec1ae05c6d2658e02ecff81963c5b3909a998e
Olesya <olesyam.miller@gmail.com>
2022-10-20 00:42:21 -0400

    create relationship between user and transaction

--- backend/app/controllers/transactions_controller.rb
+++ backend/app/controllers/transactions_controller.rb
@@ -1,7 +1,29 @@
 class TransactionsController < ApplicationController
   def index
+    transactions = Transaction.all
+    render json: transactions
   end
 
   def create
+    transaction = Transaction.new(transaction_params)
+    if transaction.save
+        render json: transaction
+    else
+        render json: { message: transaction.errors }, status: 400
+    end
   end
+
+  def transaction_params 
+    params.require(:transaction).permit(
+        :title, 
+        :user_id, 
+        :amount, 
+        :gen_ledger_account
+    )
+end
+
+  def find_transaction
+    @transaction = Transaction.find_by(id: params[:id])
+  end
+  
 end
--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -5,5 +5,11 @@ class UsersController < ApplicationController
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+        render json: user
+    else
+        render json: { message: user.errors }, status: 400
+    end
   end
 end
